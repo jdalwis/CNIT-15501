0ce997860cd78165c74cc05e7b2fabe4e78e7eb3
Janith D'Alwis <jdalwis@purdue.edu>
2024-01-27 14:43:08 -0500

    Add files via upload

---
+++ InLabs/CNIT155_InLab01.py
@@ -0,0 +1,34 @@
+#===============================================================
+# Your Name & Lab Section: Janith D'Alwis, Friday 1:30pm
+# Your Purdue Email:
+# Program Description: Program that prints out graphical representation of CNIT 155. 
+# Academic Honesty:
+# I attest that this is my original work.
+# I have not used unauthorized source code, either modified or unmodified.
+# I have not given other fellow student(s) access to my program.
+#=================================================================
+
+def main(): #defining the main function
+    print("     =================================================") #print function
+    print("     =           Name: Janith D'Alwis                =")
+    print("     =           CNIT155-Assignment 01               =")
+    print("     =================================================\n\n\n\n\n") #\n to create spacing
+    print("         ccccc      NNN          NN         IIIIIIIIII        TTTTTTTTTTTTTT ")
+    print("      cccc          NNNN         NN             II                  TT")
+    print("    ccc             NN   N       NN             II                  TT")
+    print("  ccc               NN     N     NN             II                  TT")
+    print("  ccc               NN      N    NN             II                  TT")
+    print("  ccc               NN       N   NN             II                  TT")
+    print("    ccc             NN        N  NN             II                  TT")
+    print("      cccc          NN         NNNN             II                  TT")
+    print("         ccccc      NN          NNN         IIIIIIIIII              TT\n\n\n\n\n")
+    print("             111             555555555            555555555")
+    print("            1111             55                   55")
+    print("           11 11             55                   55") 
+    print("          11  11             555555555            555555555") 
+    print("              11                   555                  555")
+    print("              11                   555                  555") 
+    print("              11                  555                  555") 
+    print("          111111111          5555555              5555555")
+
+main() #calling the main function
